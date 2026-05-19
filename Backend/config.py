1171fcf7e61fa42486728f1cb7457c413f8fd112
Anshika Jaiswal <jaiswal.anshika2016@gmail.com>
2026-05-19 08:46:06 +0530

    Force SQLite on Render

--- Backend/config.py
+++ Backend/config.py
@@ -6,23 +6,23 @@ load_dotenv()
 
 class Config:
 
-    DB_USER = os.getenv("DB_USER")
-    DB_PASSWORD = os.getenv("DB_PASSWORD")
-    DB_HOST = os.getenv("DB_HOST")
-    DB_NAME = os.getenv("DB_NAME")
-
-    if DB_USER and DB_PASSWORD and DB_HOST and DB_NAME:
-
-        password = quote_plus(DB_PASSWORD)
-
-        SQLALCHEMY_DATABASE_URI = (
-            f"mysql+pymysql://{DB_USER}:{password}@{DB_HOST}/{DB_NAME}"
-        )
-
-    else:
-
+    if os.getenv("RENDER"):
         SQLALCHEMY_DATABASE_URI = "sqlite:///traveloop.db"
 
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    else:
+        DB_USER = os.getenv("DB_USER")
+        DB_PASSWORD = os.getenv("DB_PASSWORD")
+        DB_HOST = os.getenv("DB_HOST")
+        DB_NAME = os.getenv("DB_NAME")
 
+        if DB_USER and DB_PASSWORD and DB_HOST and DB_NAME:
+            password = quote_plus(DB_PASSWORD)
+
+            SQLALCHEMY_DATABASE_URI = (
+                f"mysql+pymysql://{DB_USER}:{password}@{DB_HOST}/{DB_NAME}"
+            )
+        else:
+            SQLALCHEMY_DATABASE_URI = "sqlite:///traveloop.db"
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY", "traveloop-secret")
